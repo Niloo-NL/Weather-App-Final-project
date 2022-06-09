2198769548fca50cdcf2826c7f28ed3206d5f01d
Niloofar <niloofar.navaieelavasani@gmail.com>
2022-06-09 14:48:02 +0200

    Added current date and time

--- src/java.js
+++ src/java.js
@@ -1,15 +1,43 @@
+function timeInfo(timestamp) {
+  return "05:00";
+}
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = weekDays[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemp(response) {
   let temperature = document.querySelector("#temp");
   let cityName = document.querySelector("#city");
   let descriptionInfo = document.querySelector("#description");
   let humidityInfo = document.querySelector("#humidity");
   let windInfo = document.querySelector("#wind");
-  console.log(response);
+  let dateElement = document.querySelector("#date");
   temperature.innerHTML = Math.round(response.data.main.temp);
   cityName.innerHTML = response.data.name;
   descriptionInfo.innerHTML = response.data.weather[0].description;
   humidityInfo.innerHTML = response.data.main.humidity;
-  windInfo.innerHTML = Math.round(response.data.main.wind.speed);
+  windInfo.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let cityName = "Berlin";
